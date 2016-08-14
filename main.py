3208e04614894ff9a16ba872596ad7dccb1e5c40
Yury Mironov <yuriymironov96@gmail.com>
2016-08-14 15:18:08 +0300

    Error handlers with templates added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, redirect, abort, render_template
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager =  Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def homepage():
@@ -30,6 +32,18 @@ def admin(name):
     if name != 'Yury':
         abort(403)
     return render_template('admin.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(403)
+def page_not_found(e):
+    return render_template('403.html'), 403
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     manager.run()
