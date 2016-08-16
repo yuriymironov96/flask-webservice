c6fd3d61f435ef4ea980d6a4b5c82d107154b2de
Yury Mironov <yuriymironov96@gmail.com>
2016-08-16 21:29:10 +0300

    Added email change

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -53,3 +53,9 @@ class NewPasswordForm(Form):
     def validate_email(self, field):
         if User.query.filter_by(email=field.data).first() is None:
             raise ValidationError('Unknown email address.')
+
+class ChangeEmailForm(Form):
+    email = StringField('New email', validators=[Required(), Length(1, 64),
+                                                 Email()])
+    password = PasswordField('Password', validators=[Required()])
+    submit = SubmitField('Update Email Address')
