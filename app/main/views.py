49d9cb2811f93e4308450fa20654469005003dd1
Yury Mironov <yuriymironov96@gmail.com>
2016-08-19 15:22:07 +0300

    Added user profiles and their edition

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,12 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, current_app, request, abort
+from flask import render_template, session, redirect, url_for, current_app, request, abort, flash
 from . import main
-from .forms import NameForm
+from .forms import NameForm, EditProfileForm, EditProfileAdminForm
 from .. import db
-from ..models import User
+from ..models import User, Role
 from ..email import send_email
-from flask.ext.login import login_required
+from flask.ext.login import login_required, current_user
+from ..decorators import admin_required
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
@@ -30,10 +31,6 @@ def any_page(pagename):
     user_agent = request.headers.get('User-Agent')
     return render_template('user_agent.html', pagename=pagename, user_agent=user_agent)
 
-@main.route('/user/<username>')
-def any_user(username):
-    return render_template('user.html', username=username)
-
 @main.route('/app/<anything>')
 def anything(anything):
     return render_template('not_found.html')
@@ -52,3 +49,50 @@ def admin(name):
 @login_required
 def secret():
     return 'Only authenticated users are allowed!'
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user.html', user=user)
+
+@main.route('/edit-profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user', username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', form=form)
+
+@main.route('/edit-profile/<int:id>', methods=['GET', 'POST'])
+@login_required
+@admin_required
+def edit_profile_admin(id):
+    user = User.query.get_or_404(id)
+    form = EditProfileAdminForm(user=user)
+    if form.validate_on_submit():
+        user.email = form.email.data
+        user.username = form.username.data
+        user.confirmed = form.confirmed.data
+        user.role = Role.query.get(form.role.data)
+        user.name = form.about_me.data
+        user.location = form.location.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        flash('The profile has been updated')
+        return redirect(url_for('.user', username=user.username))
+    form.email.data = user.email
+    form.username.data = user.username
+    form.confirmed.data = user.confirmed
+    form.role.data = user.role_id
+    form.name.data = user.name
+    form.location.data = user.location
+    form.about_me.data = user.about_me
+    return render_template('edit_profile.html', form=form, user=user)
--- app/models.py
+++ app/models.py
@@ -4,6 +4,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
 from flask.ext.login import UserMixin, AnonymousUserMixin
+from datetime import datetime
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -53,6 +54,11 @@ class User(UserMixin, db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
+    name = db.Column(db.String(64))
+    location = db.Column(db.String(64))
+    about_me = db.Column(db.Text())
+    member_since = db.Column(db.DateTime(), default=datetime.utcnow)
+    last_seen = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
@@ -136,6 +142,10 @@ class User(UserMixin, db.Model):
 
     def is_administrator(self):
         return self.can(Permission.ADMINISTER)
+
+    def ping(self):
+        self.last_seen = datetime.utcnow()
+        db.session.add(self)
 
 class AnonymousUser(AnonymousUserMixin):
 
